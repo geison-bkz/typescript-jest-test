74ac582a804baae258da1dc074ab25ea83e04614
geisondossantos <geison_bf@hotmail.com>
2022-07-18 09:26:12 -0300

    Update Persistency.spec.ts

--- src/services/Persistency.spec.ts
+++ src/services/Persistency.spec.ts
@@ -1,13 +1,24 @@
-describe('TESTANDO ALGUMA COISA', () => {
-  it('descrição do teste(IT)', () => {
-    const number = 1;
-    expect(number).toBe(1);
-  });
-});
+import { Persistency } from './Persistency';
 
-describe('TESTANDO ALGUMA COISA', () => {
-  test('descrição do teste(TEST)', () => {
-    const nome = 'Geison';
-    expect(nome).toBe('Geison');
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
   });
 });
